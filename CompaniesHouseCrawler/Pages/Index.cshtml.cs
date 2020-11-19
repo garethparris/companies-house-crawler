669e86e687e8dcd11b297f63a84108eb31a7269d
Gareth Parris <gareth@prime23.co.uk>
2020-11-19 21:53:25 +0000

    Basic CH REST API quer

---
+++ CompaniesHouseCrawler/Pages/Index.cshtml.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+
+using CompaniesHouseCrawler.Models;
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+using RestSharp;
+using RestSharp.Authenticators;
+
+namespace CompaniesHouseCrawler.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private const string SearchUrlBase = "https://find-and-update.company-information.service.gov.uk/search?q=";
+
+        private readonly ILogger<IndexModel> logger;
+        private readonly IConfiguration configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+        {
+            this.logger = logger;
+            this.configuration = configuration;
+        }
+
+        public string Name { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPostSubmit(SearchModel search)
+        {
+            this.Name = search.Name;
+
+            var apiKey = this.configuration["APIKey"];
+
+            var client = new RestClient("https://api.company-information.service.gov.uk")
+            {
+                Authenticator = new HttpBasicAuthenticator(apiKey, string.Empty)
+            };
+
+            var request = new RestRequest("search/officers", Method.GET).AddParameter("q", this.Name);
+            var response = client.Execute(request);
+
+            //https://api.company-information.service.gov.uk/search/officers?q=douglass+barrowman
+
+            var json = response.Content;
+        }
+    }
+}
